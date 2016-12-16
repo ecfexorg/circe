8927ad818198bad2ab7769b39504374e9acd5045
varHarrie <varharrie@gmail.com>
2016-12-16 15:46:52 +0800

    add Circe constructor, export Koa and KoaRouter

--- src/circe.js
+++ src/circe.js
@@ -1,3 +1,32 @@
-class Circe {}
+const http = require('http')
+const Koa = require('koa')
+const Router = require('koa-router')
+
+class Circe {
+  /**
+   * Creates an instance of Circe.
+   *
+   * @api public
+   */
+  constructor () {
+    this.app = new Koa()
+    this.server = http.createServer(this.app.callback())
+  }
+
+  /**
+   * Start and listen on port
+   *
+   * @param {any} port
+   * @param {any} callback
+   *
+   * @api public
+   */
+  listen (port, callback) {
+    this.server.listen.apply(this.server, arguments)
+  }
+}
+
+Circe.Koa = Koa
+Circe.Router = Router
 
 module.exports = Circe
