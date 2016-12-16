877b709b30b6dfaf0a22b5a2d9650da1f7012666
varHarrie <varharrie@gmail.com>
2016-12-16 17:24:46 +0800

    add methods: route, inject

--- src/circe.js
+++ src/circe.js
@@ -1,7 +1,12 @@
 const http = require('http')
+const path = require('path')
+
 const Koa = require('koa')
 const Router = require('koa-router')
 const delegates = require('delegates')
+const requireDir = require('require-dir')
+const debug = require('debug')('circe:application')
+const _ = require('lodash')
 
 class Circe {
   /**
@@ -24,6 +29,57 @@ class Circe {
    */
   listen (port, callback) {
     this.server.listen.apply(this.server, arguments)
+  }
+
+  /**
+   * load routers
+   *
+   * @param {String|Router} arg
+   * @returns instance
+   *
+   * @api public
+   */
+  route (arg) {
+    if (arg instanceof Router) {
+      this.use(arg.routes())
+      this.use(arg.allowedMethods())
+    } else if (typeof arg === 'string') {
+      const apiDir = path.resolve(path.dirname(module.parent.filename), arg)
+      debug('apis directory: %s', apiDir)
+      const routers = requireDir(apiDir)
+      for (let key in routers) {
+        let router = routers[key]
+        this.use(router.routes())
+        this.use(router.allowedMethods())
+      }
+    } else {
+      throw new Error('Can not load routes from: ' + arg)
+    }
+    return this
+  }
+
+  /**
+   * pass variables into app.context
+   *
+   * @param {Object} variables
+   * @param {String} prefix
+   * @returns instance
+   *
+   * @api public
+   */
+  inject (arg1, arg2) {
+    if (typeof arg1 === 'object') {
+      const services = arg1
+      const prefix = typeof arg2 === 'string' ? arg2.trim() : ''
+      for (let name in services) {
+        debug('inject %j to context.%s', services[name], prefix + name)
+        _.set(this.app.context, prefix + name, services[name])
+      }
+    } else if (arg1 && typeof arg1 === 'string' && arg2) {
+      debug('inject %j to context.%s', arg2, arg1)
+      _.set(this.app.context, arg1, arg2)
+    }
+    return this
   }
 }
 
