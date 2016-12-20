c115cecfc60dcb148ebf1b3cee798a3b5b273eaa
varHarrie <varharrie@gmail.com>
2016-12-20 16:54:17 +0800

    add jwtVerifier middleware

---
+++ src/middlewares/jwtVerifier/index.js
@@ -0,0 +1,60 @@
+const {sign, verify, decode} = require('jsonwebtoken')
+const unless = require('../unless')
+
+const signAsync = function (payload, secret, options = {}) {
+  return new Promise((resolve, reject) => {
+    sign(payload, secret, options, function (err, token) {
+      if (err) reject(err)
+      else resolve(token)
+    })
+  })
+}
+
+const verifyAsync = function (accessToken, secret, options = {}) {
+  return new Promise((resolve, reject) => {
+    verify(accessToken, secret, options, function (err, decodedToken) {
+      if (err) reject(err)
+      else resolve(decodedToken)
+    })
+  })
+}
+
+function jwtVerifier (options = {}) {
+  if (!options.secret) throw new Error('missing secret')
+
+  const secret = options.secret
+  const key = options.key || 'user'
+
+  function middleware (ctx, next) {
+    const accessToken = resolveHeader(ctx)
+    return verifyAsync(accessToken, secret).then((decodedToken) => {
+      ctx.state = ctx.state || {}
+      ctx.state[key] = decodedToken
+    }).catch((err) => {
+      ctx.throw(401, `Invalid token - ${err.message}`)
+    }).then(() => next())
+  }
+
+  middleware.unless = unless
+  return middleware
+}
+
+function resolveHeader (ctx) {
+  if (!ctx.header || !ctx.header.authorization) {
+    throw new Error('can\'t find authorization header')
+  }
+  const parts = ctx.header.authorization.split(' ')
+  if (parts.length === 2) {
+    const [scheme, credentials] = parts
+    if (/^Bearer$/i.test(scheme)) return credentials
+  }
+  throw new Error('Authorization header format is "Authorization: Bearer token"')
+}
+
+jwtVerifier.sign = sign
+jwtVerifier.signAsync = signAsync
+jwtVerifier.verify = verify
+jwtVerifier.verifyAsync = verifyAsync
+jwtVerifier.decode = decode
+
+module.exports = jwtVerifier
